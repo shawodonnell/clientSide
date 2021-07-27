75c8017817c8b6cf09d52420ccd4d66c4b0c15bd
Shaw ODonnell <shawodonnell@googlemail.com>
2021-07-27 17:21:11 +0100

    axios changed server to numbers

--- tap.js
+++ tap.js
@@ -1,6 +1,6 @@
 //document.cookie = "WhyteGoodMan=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImM5NGJkNzFiLWQ5YTEtNGZlOC05NTRhLTQ1YWQ4MzdmZmQ4NSIsImlhdCI6MTYyNzMxODM2OX0.o9pua8PTeM2OBcy366q-Aulf_fSLlCZEKJfA1mhh7K0"
-const socket = io("http://localhost:3000");
-const cartSocket = io("http://localhost:3000/api/v1/cart")
+//const socket = io("http://localhost:3000");
+//const cartSocket = io("http://localhost:3000/api/v1/cart")
 let cartID;
 
 //simulating logging and cookie being loaded into browser
@@ -12,7 +12,7 @@ document.querySelector("#cookie").addEventListener("click", async () => {
 
 document.querySelector("#cart").addEventListener("click", async () => {
 
-    const url = "http://localhost:3000/api/v1/cart";
+    const url = "http://127.0.0.1:3000/api/v1/cart";
     //const url = "http://localhost:2000/";
     let data = {
         fingerprint: "de4b27d8beca3167f9ec694d76aa5a35",
@@ -33,7 +33,7 @@ document.querySelector("#cart").addEventListener("click", async () => {
     }
     data = JSON.stringify(data);
 
-    await axios.post("http://localhost:3000/api/v1/cart",{
+    await axios.post("http://127.0.0.1:3000/api/v1/cart",{
         title:"cartObject",
         fingerprint: "de4b27d8beca3167f9ec694d76aa5a35"
     },{withCredentials: true})
