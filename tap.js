12aeb27a4b7df59ccad99ea043a732f6095a4eaa
Shaw ODonnell <shawodonnell@googlemail.com>
2021-07-29 12:05:19 +0100

    delete function

--- tap.js
+++ tap.js
@@ -4,19 +4,42 @@ let cartID;
 let fingerprint = "de4b27d8beca3167f9ec694d76aa5a35";
 let userID = "60f85a5ecf06402d10247601"
 
+//SOCKET EVENT LISTENERS
 socket.on("connect",()=>{
   console.log("New Server Connection",socket.id);
 })
 
-//simulating logging and cookie being loaded into browser
+socket.on("failedUserAuth",(data)=>{
+  alert(data)
+  window.open("www.google.com",_blank)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+//PAGE EVENT LISTENERS
+
+//simulating logging and cookie being loaded into browser*********************************
 document.querySelector("#cookie").addEventListener("click", async () => {
   let url = `http://127.0.0.1:3000/api/v1/users/${userID}`;
       
   await axios.get(url,{withCredentials:true}).then(response=>console.log(response))
 })
 
-//NEW CART/PURCHASE
+//NEW CART/PURCHASE********************************************
 document.querySelector("#cart").addEventListener("click", async () => {
+
+  
+  document.querySelector("#cartAudio").playAudio("https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3");
 
   socket.on("cartID",(data)=>{
     cartID = data;
@@ -33,6 +56,7 @@ document.querySelector("#cart").addEventListener("click", async () => {
 
   socket.on("orderComplete",(data)=>{
     console.log("order Completed",data);
+
   })
 
   socket.on("responseIncoming",(data)=>{
@@ -72,7 +96,7 @@ document.querySelector("#cart").addEventListener("click", async () => {
 
 })
 
-//AMENDING A CART
+//AMENDING A CART**************************************************
 document.querySelector("#update").addEventListener("click", async () => {
 
   socket.on("cartAmended",(data)=>{
@@ -126,8 +150,49 @@ document.querySelector("#update").addEventListener("click", async () => {
 
 })
 
+//DELETING A CART**************************************************
+document.querySelector("#delete").addEventListener("click", async () => {
 
+  socket.on("deletingCart",(data)=>{
+    console.log("deleting cart...",data);
+  })
 
+  socket.on("timerStopped",(data)=>{
+    console.log("timer:",data);
+  })
+
+  socket.on("cartDeleted",(data)=>{
+    console.log("Deleted...",data);
+  })
+
+  socket.on("deleteError",(data)=>{
+    console.log("ERROR...",data);
+  })
+
+  socket.on("disconnectWS",(data)=>{
+    console.log("Disconnecting...", data);
+    socket.disconnect();
+    socket = null
+    socket = io("http://127.0.0.1:3000");
+    socket.connect();        
+  })
+
+  await axios.delete("http://127.0.0.1:3000/api/v1/cart",{
+      cartID:cartID,
+      userID:userID,
+  })
+  .then(response=>{
+    console.log("DELETE RESPONSE",response)
+  })
+  .catch(err=>console.log(err))
+
+})
+
+//FUNCTIONS
+function play() {
+  var audio = document.getElementById("audio");
+  audio.play();
+}
 
   // socket.emit("message","Hello from client")
   // socket.on("newMessage",(message)=>{
